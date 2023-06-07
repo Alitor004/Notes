2d21913d6b09b98d4be7cd70d98622fc987d8114
Alitor <irmaosgomes1302@gmail.com>
2023-06-07 20:51:58 -0300

    Finalização com adção de notas

---
+++ PAM_MAUI_App/Models/About.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PAM_MAUI_App.Models;
+
+internal class About
+{
+    public string Title => AppInfo.Name;
+    public string Version => AppInfo.VersionString;
+    public string MoreInfoUrl => "https://aka.ms/maui";
+    public string Message => "Este App esta escrito em XAML e C# com .NET MAUI.";
+}
---
+++ PAM_MAUI_App/Models/Note.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PAM_MAUI_App.Models;
+
+internal class Note
+{
+    public string Filename { get; set; }
+    public string Text { get; set; }
+    public DateTime Date { get; set; }
+}
