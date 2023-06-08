4b7400c48e8442afc710f894c79eb896f70e5fe0
Alitor <irmaosgomes1302@gmail.com>
2023-06-07 22:41:02 -0300

    Começo da segunda parte

---
+++ PAM_MAUI_App/ViewModels/AboutViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PAM_MAUI_App.ViewModels
+{
+    class AboutViewModel
+    {
+    }
+}
