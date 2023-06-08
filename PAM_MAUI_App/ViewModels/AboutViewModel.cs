3dab485fa67e5754923dccfb509d69eb5a53e804
Alitor <irmaosgomes1302@gmail.com>
2023-06-07 22:41:52 -0300

    Começo da segunda parte

--- PAM_MAUI_App/ViewModels/AboutViewModel.cs
+++ PAM_MAUI_App/ViewModels/AboutViewModel.cs
@@ -3,10 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.Input;
+using System.Windows.Input;
 
-namespace PAM_MAUI_App.ViewModels
+namespace PAM_MAUI_App.ViewModels;
+
+internal class AboutViewModel
 {
-    class AboutViewModel
+    public string Title => AppInfo.Name;
+    public string Version => AppInfo.VersionString;
+    public string MoreInfoUrl => "https://aka.ms/maui";
+    public string Message => "This app is written in XAML and C# with .NET MAUI.";
+    public ICommand ShowMoreInfoCommand { get; }
+
+    public AboutViewModel()
     {
+        ShowMoreInfoCommand = new AsyncRelayCommand(ShowMoreInfo);
     }
+
+    async Task ShowMoreInfo() =>
+        await Launcher.Default.OpenAsync(MoreInfoUrl);
 }
